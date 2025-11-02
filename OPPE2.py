87837aaab51558cae4540db48dbae7858a3932ee
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-02 21:29:53 +0530

    question statement 2

---
+++ OPPE2.py
@@ -0,0 +1,16 @@
+# question statement
+
+
+# Is Odd-Length Palindrome
+# Write a function is_odd_length_palindrome that takes a string s and returns True if s is a palindrome of odd length, and False otherwise.
+
+# NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+# Example
+
+#     • "hello" --> False (not a palindrome)
+
+#     • "noon" --> False (palindrome but not odd length)
+
+#     • "nun" --> True (palindrome and odd length)
+
