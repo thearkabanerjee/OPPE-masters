a22ccb2a817b7a25d8a15591f824168562dba47d
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-03 10:28:04 +0530

    exercise 1

---
+++ EXERCISES/WEEK-1/wecando.py
@@ -0,0 +1,23 @@
+# question statement:
+
+# output1 = ... # int: sum of a and b
+# output2 = ... # int: twice the sum of a and b
+# output3 = ... # int: absolute difference between a and b
+# output4 = ... # int: absolute difference between sum and product of a and b
+
+# # Find discounted price given price and discount_percent
+# # input variables : price: int, discount_percent: float
+# discounted_price = ... # float
+
+# # Round the discounted_price
+# rounded_discounted_price = ... # int
+
+# # Find hrs and mins given the total_mins
+# # input variables : total_mins
+# hrs = ... # int: hint: think about floor division operator
+# mins = ... # int
+
+
+
+
+
