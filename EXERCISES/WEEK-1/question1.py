5725320820acb7b2dec51a443ef4022d0f784c49
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-03 11:16:40 +0530

    question 1

--- EXERCISES/WEEK-1/question1.py
+++ EXERCISES/WEEK-1/question1.py
@@ -37,8 +37,12 @@ output4 = abs (output1 - (a*b))
 
 discounted_price = float((100- discount_percent) * price)
 
+rounded_discounted_price = abs (int (discounted_price))
+
 
 print (discounted_price, output4, output3, output2, output1)
+
+print (rounded_discounted_price)
 
 
 
