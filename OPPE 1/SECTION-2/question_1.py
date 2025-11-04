90ec01956215bbfad4d120ea9c206af9bf9f6de2
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-04 13:29:17 +0530

    question statement

--- OPPE 1/SECTION-2/question_1.py
+++ OPPE 1/SECTION-2/question_1.py
@@ -18,3 +18,14 @@
 
 
 
+def sum_of_squares_of_even(nums: list) -> int:
+    '''Return the sum of squares of all even numbers in the list.
+
+    Args:
+        nums : list - list of integers
+
+    Returns:
+        int - sum of squares of all even numbers
+    '''
+    ...
+    
