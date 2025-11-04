9102d6c3bbde0d16c8e06eb7fe8ff2236f7e87b3
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-04 13:28:02 +0530

    question 1 section 2

---
+++ OPPE 1/SECTION-2/question_1.py
@@ -0,0 +1,20 @@
+# Sum of Squares of Even Numbers
+# Write a function sum_of_squares_of_even
+# that takes a list of integers nums and returns
+# the sum of squares of all even numbers in the list.
+
+# >>> nums = [1, 2, 3, 4, 5, 6]
+# >>> sum_of_squares_of_even(nums)
+# 56
+# NOTE: This is a function type question,
+# you don't have to take input or print the output,
+# just have to complete the required function definition.
+
+# Explanation
+# In the list [1, 2, 3, 4, 5, 6],
+# the even numbers are 2, 4, and 6.
+# The sum of their squares is 4 + 16 + 36 = 56.
+
+
+
+
