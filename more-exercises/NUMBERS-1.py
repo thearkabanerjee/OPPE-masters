3d9d52e3724aedc3c920e7d5e1a323acf882d917
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-05 10:40:21 +0530

    numbers-1.py

--- more-exercises/NUMBERS-1.py
+++ more-exercises/NUMBERS-1.py
@@ -15,23 +15,30 @@
 
 # template code 
 
-def percentage_increased(original, new):
-    '''Calculate the percentage increase from the original value to the new value.
+# def percentage_increased(original, new):
+#     '''Calculate the percentage increase from the original value to the new value.
 
-    Assume original is less than or equal to new.
+#     Assume original is less than or equal to new.
 
-    Args:
-        original (float): The original value.
-        new (float): The new value.
+#     Args:
+#         original (float): The original value.
+#         new (float): The new value.
 
-    Returns:
-        float: The percentage increase.
+#     Returns:
+#         float: The percentage increase.
 
-    Examples:
-    >>> percentage_increased(50, 75)
-    50.0
-    >>> percentage_increased(80, 100)
-    25.0
-    '''
-    ...
+#     Examples:
+#     >>> percentage_increased(50, 75)
+#     50.0
+#     >>> percentage_increased(80, 100)
+#     25.0
+#     '''
+#     ...
     
+
+
+def percentage_increased(original, new):
+    the_difference = (new - original)
+    the_percentage_increased = ((the_difference/original) * 100)
+
+    return (the_percentage_increased)
