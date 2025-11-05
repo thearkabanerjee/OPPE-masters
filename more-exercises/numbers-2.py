3fde0c56e2e84b9792865f29964454ef7c8e23dd
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-05 10:44:50 +0530

    question-2

--- more-exercises/numbers-2.py
+++ more-exercises/numbers-2.py
@@ -16,3 +16,30 @@
 # >>> is_ten_digit_even(-9289479278)
 # True
 
+
+#template code 
+
+# def is_ten_digit_even(n):
+#     '''Checks if a number is a 10 digit even number.
+
+#     Also account for negative numbers discarding the sign.
+
+#     Args: 
+#         n (int): The given number. 
+    
+#     Returns: 
+#         bool : result as True or False. 
+    
+#     Examples:
+#     >>> is_ten_digit_even(8769473839)
+#     False
+#     >>> is_ten_digit_even(928948)
+#     False
+#     >>> is_ten_digit_even(9289479278)
+#     True
+#     >>> is_ten_digit_even(-9289479278)
+#     True
+#     '''
+#     ...
+    
+
