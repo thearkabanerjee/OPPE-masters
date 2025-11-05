be86b730f8084d6a57e601efc976fafd43313d4f
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-05 21:20:49 +0530

    strings-1

--- more-exercises/strings-1.py
+++ more-exercises/strings-1.py
@@ -16,3 +16,23 @@
 # '2, 1'
 
 
+# template code 
+
+
+# def format_as_second_comma_first(t: tuple) -> str:
+#     '''Formats the two elements in a tuple as "second, first".
+
+#     Arguments:
+#     t: tuple - a tuple two elements
+
+#     Return:
+#     str - a formatted string "second, first"
+
+#     Example:
+#     >>> format_as_second_comma_first(('hello', 'python'))
+#     'python, hello'
+#     >>> format_as_second_comma_first((1, 2))
+#     '2, 1'
+#     '''
+#     ...
+    
