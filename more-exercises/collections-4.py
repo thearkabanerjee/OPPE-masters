da93918716c6d8e5dc97f7801d0521b031585b67
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-11-05 22:15:57 +0530

    collections question 1

---
+++ more-exercises/collections-4.py
@@ -0,0 +1,45 @@
+# problem statement :
+
+
+# Set of Unique vowels in a string.
+# Given a string, return a set of unique vowels present in the string.
+
+# Examples
+
+# >>> unique_vowels('banana treat')
+# {'a', 'e'}
+# >>> unique_vowels('apple lolipop')
+# {'a', 'e', 'i', 'o'}
+
+
+
+
+# def unique_vowels(s: str) -> set:
+#     '''
+#     Given a string, return a set of unique vowels present in the string.
+
+#     Arguments:
+#     s: str - the input string
+
+#     Return:
+#     set - a set of unique vowels present in the string
+
+#     Examples:
+#     >>> unique_vowels('banana treat')
+#     {'a', 'e'}
+#     >>> unique_vowels('apple lolipop')
+#     {'a', 'e', 'i', 'o'}
+#     >>> unique_vowels('Ian Avinkov')
+#     {'I','A','a','i','o'}
+#     '''
+#     ...
+    
+# def unique_vowels(s:str) -> set:
+#     # vowels = ("a","e","i","o","u", "A", "E","I","O","U")
+#     vowels = ("aeiouAEIOU")
+#     for vowels in s:
+
+
+
+
+# find out later
